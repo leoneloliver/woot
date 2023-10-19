541544e95d4c7565f0ca55e449c69ca0e1d7c0e6
Leonel Oliveira <leonel.oliveira@volarisgroup.com>
2023-10-19 17:36:21 -0400

    Style changes

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ export default function Document() {
       
       <Head> 
 
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> */}
       <link
           rel="stylesheet"
           type="text/css"
