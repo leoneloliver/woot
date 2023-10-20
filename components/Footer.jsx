69d5b6ac1b02be84087d771282b603af3dc9ba90
Leonel Oliveira <leonel.oliveira@volarisgroup.com>
2023-10-20 17:16:40 -0400

    Copy changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,41 +9,38 @@ export default function Footer() {
             <nav>
                 <h2>Company Info</h2>
                 <ul>
-                    <li><a href="https://www.woot.com/about?ref=w_ngf_abt">About Us</a></li>
-                    <li><a href="https://www.woot.com/jobs?ref=w_ngf_wfw">Careers</a></li>
-                    <li><a href="https://www.woot.com/faq?ref=w_ngf_faqs">FAQ</a></li>
-                    <li><a href="https://www.woot.com/feedback?ref=w_ngf_fdbk">Feedback</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Feedback</a></li>
                 </ul>
             </nav>
             <nav>
                 <h2>Customer Care</h2>
                 <ul>
-                    <li><a href="https://account.woot.com/support?ref=w_ngf_spt">Customer Service</a></li>
-                    <li><a href="https://www.woot.com/faq?ref=w_ngf_rp#what-is-woots-return-policy">Woot's Return Policy</a></li>
-                    <li><a href="https://www.woot.com/warranty?ref=w_ngf_pw">Product Warranty</a></li>
-                    <li><a href="https://www.woot.com/recalls?ref=w_ngf_pdn">Product Recall Notices</a></li>
-                    <li><a href="https://www.woot.com/writeus?ref=w_ngf_wtus">Write Us</a></li>
+                    <li><a href="#">Customer Service</a></li>
+                    <li><a href="#">Woot's Return Policy</a></li>
+                    <li><a href="#">Product Warranty</a></li>
+                    <li><a href="#">Product Recall Notices</a></li>
                 </ul>
             </nav>
             <nav>
                 <h2>Community</h2>
                 <ul>
-                    <li><a href="https://www.facebook.com/woot" data-external-ref-tag="w_ngf_fb" target="_blank">Facebook</a></li>
-                    <li><a href="https://twitter.com/woot" data-external-ref-tag="w_ngf_twit" target="_blank">Twitter</a></li>
-                    <li><a href="https://forums.woot.com/">Forums</a></li>
-                    <li><a href="https://forums.woot.com/c/everything-but-woot">Everything But Woot</a></li>
-                    <li><a href="https://www.woot.com/blog?ref=w_ngf_blg">Blog</a></li>
-                    <li><a href="https://developer.woot.com/" data-external-ref-tag="w_ngf_dev">Developer Portal</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Developer Portal</a></li>
                 </ul>
             </nav>
             <nav>
                 <h2>Boring Stuff</h2>
                 <ul>
-                    <li><a href="https://www.woot.com/minions?ref=w_ngf_min">Woot Affiliates</a></li>
-                    <li><a href="https://www.woot.com/terms?ref=w_ngf_tc">Terms and Conditions</a></li>
-                    <li><a href="https://www.woot.com/Prop65?ref=w_ngf_p65">Prop 65</a></li>
-                    <li><a href="https://www.woot.com/privacy?ref=w_ngf_pp">Privacy Policy</a></li>
-                    <li><a href="https://vendorportal.woot.com" data-external-ref-tag="w_ngf_vp">Vendor Resources</a></li>
+                    <li><a href="#">Woot Affiliates</a></li>
+                    <li><a href="#">Terms and Conditions</a></li>
+                    <li><a href="#">Prop 65</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+
                 </ul>
             </nav>
             
