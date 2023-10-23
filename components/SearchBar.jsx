89a4356628c5a66e8b1a6c730badbff4e3b15b81
Leonel Oliveira <leonel.oliveira@volarisgroup.com>
2023-10-23 17:13:37 -0400

    Small changes to the search bar

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -20,27 +20,10 @@ const SearchBar = () => {
                 Search
             </label>
             <div className="relative max-w-screen-xl">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg
-                        className="w-4 h-4 text-gray-500 "
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 20"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                        />
-                    </svg>
-                </div>
                 <input
                     type="search"
                     id="default-search"
-                    className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500"
+                    className="block w-full p-3 pl-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500"
                     placeholder="Search Products..."
                     required="required"
                     value={keyword}
